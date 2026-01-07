d285dd47f3ac61e7fe1100be5239745326c46d54
mohya972 <mohya972n@gmail.com>
2026-01-07 12:26:31 -0400

    Syntaxe JSX 02

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+
 function App() {
-  return <div>
+  return <>
             <h1 id="title" className="title">Bonjour les gens !</h1>  
             <p> En plein tuto, ma fille souhaite que je l'a récupère. </p>
             <p> Pourtant je débute à peine l'exercice. Que faire !?</p>
-          </div>
-  
+          </>
 }
 
 export default App
