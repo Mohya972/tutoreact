98b402212d10930d691d830212694d7c09c68cc6
mohya972 <mohya972n@gmail.com>
2026-01-07 12:58:41 -0400

    Syntaxe JSX 04

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-const title = "Bonjour les <strong> Amours </strong> !"
+const title = "Bonjour les Amours !"
+const style = { color: "red", backgroundColor: "yellow" }
 
 function App() {
   return <>
-            <h1 id="title" className="title" dangerouslySetInnerHTML={{__html: title}}></h1>  
-            <p> En plein tuto, ma fille souhaite que je l'a récupère. </p>
+            <h1 id="title" className="title" style={style}>{title}</h1>  
+            
+            <input type="text" placeholder="Me voiciii !!!" />
+            
+            <p> En plein tuto, ma fille souhaite que je la récupère. </p>
             <p> Pourtant je débute à peine l'exercice. Que faire !?</p>
           </>
 }
