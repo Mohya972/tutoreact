5979eca47e6467e8e93bb803e301d6316c0b27ee
mohya972 <mohya972n@gmail.com>
2026-01-07 13:42:13 -0400

    Syntaxe JSX 07

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 const title = "Bonjour les Amours !";
 const style = { color: "red", backgroundColor: "yellow" };
 const showTitle = true;
+const todos = [
+  'Présenter React',
+  'Présenter JSX',
+  'Créer des composants'
+];
 
 function App() {
 
@@ -13,6 +18,11 @@ function App() {
             
             <p> En plein tuto, ma fille souhaite que je la récupère. </p>
             <p> Pourtant je débute à peine l'exercice. Que faire !?</p>
+            <ul>
+              {todos.map(todo => (
+                <li key={todo}> {todo} </li>
+              ))}
+            </ul>
           </>
 }
 
