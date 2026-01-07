e6af2da42164f8b0418920b071de5fec40aac493
mohya972 <mohya972n@gmail.com>
2026-01-07 13:31:13 -0400

    Syntaxe JSX 06

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 const title = "Bonjour les Amours !";
 const style = { color: "red", backgroundColor: "yellow" };
+const showTitle = true;
 
 function App() {
 
-  const handleClic = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    
-    alert("J'ai cliqué sur le titre !")
-  };
-
   return <>
-            <h1 id="title" className="title" style={style} onClick={handleClic}>{title}</h1>  
+            {showTitle ? 
+              <h1 id="title" className="title" style={style}>{title}</h1> : 
+              <p>Démo</p>  
+            }  
             <input type="text" placeholder="Me voiciii !!!" />
             
             <p> En plein tuto, ma fille souhaite que je la récupère. </p>
