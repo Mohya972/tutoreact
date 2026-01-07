4568f46e29125a0c2ec828a1f1ea80d34eb0bd7d
mohya972 <mohya972n@gmail.com>
2026-01-07 15:00:51 -0400

    Formulaire 02

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,14 @@ import { useState } from 'react';
 
 function App() {
 
-const [firstname, setFirstname] = useState('John Doe');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(new FormData(event.target));
+  }
 
-const handleChange = (e) => {
-  setFirstname(e.target.value);
-}
-
-const reset = () => {
-  setFirstname('');
-}
-
-  return <form>
-            <input type="text" name="Firstname" value={firstname} onChange={handleChange} />
-            <p>Your firstname is: {firstname}</p>
-            <button type="button" onClick={reset}>Reset</button>
+  return <form onSubmit={handleSubmit}>
+            <input type="text" name="Firstname" defaultValue="Your Firstname"/>
+            <button>Envoyer</button>
           </form>
 }
 
