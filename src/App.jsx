97491ef4dca1acd9835b3bff8469bab45226ee97
mohya972 <mohya972n@gmail.com>
2026-01-07 14:30:54 -0400

    Hook 02

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import { useState } from 'react';
 
 function App() {
 
-  const [count, setCount] = useState(0);
+  const [person, setPerson] = useState({
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 18
+  });
+
   const increment = () => {
-    setCount(count => (count + 1));
+    setPerson({
+      ...person,
+      age: person.age + 1
+    });
   }
 
   return <>
-            <p>Compteur: {count}</p>
-            <button onClick={increment}>Incrémenter</button>
+            <p>Age de {person.firstName} {person.lastName}: {person.age}</p>
+            <button onClick={increment}>Gagner une année</button>
           </>
 }
 
