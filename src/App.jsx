ea9f7463d8fe6e0c06bde215a18962d033c07ebd
mohya972 <mohya972n@gmail.com>
2026-01-07 12:34:39 -0400

    Syntaxe JSX 03

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+const title = "Bonjour les <strong> Amours </strong> !"
 
 function App() {
   return <>
-            <h1 id="title" className="title">Bonjour les gens !</h1>  
+            <h1 id="title" className="title" dangerouslySetInnerHTML={{__html: title}}></h1>  
             <p> En plein tuto, ma fille souhaite que je l'a récupère. </p>
             <p> Pourtant je débute à peine l'exercice. Que faire !?</p>
           </>
