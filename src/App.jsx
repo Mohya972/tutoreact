ffbf3e01de92559dd014a1d7e123913cccf2e560
mohya972 <mohya972n@gmail.com>
2026-01-07 14:18:52 -0400

    Compteur, Hook 01

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
-const title = "Bonjour les Amours !";
-const style = { color: "red", backgroundColor: "yellow" };
-const showTitle = true;
-const todos = [
-  'Présenter React',
-  'Présenter JSX',
-  'Créer des composants'
-];
+import { useState } from 'react';
 
 function App() {
 
-  return <>
-            <Title color="green"hidden> Mon composant </Title>
-            <input type="text" placeholder="Me voiciii !!!" />
-            
-            <p> En plein tuto, ma fille souhaite que je la récupère. </p>
-            <p> Pourtant je débute à peine l'exercice. Que faire !?</p>
-            <ul>
-              {todos.map(todo => (
-                <li key={todo}> {todo} </li>
-              ))}
-            </ul>
-          </>
-}
-
-function Title({color , children, hidden}) {
-  if (hidden) {
-    return null;
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count => (count + 1));
   }
-  return <h1 style={{ color: color }}>{children}</h1>
+
+  return <>
+            <p>Compteur: {count}</p>
+            <button onClick={increment}>Incrémenter</button>
+          </>
 }
 
 export default App;
